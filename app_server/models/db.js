db1496b4fc6c834fdcb4b29960dafc6e38054912
kjsapple <21eg106a57@anurag.edu.in>
2023-10-05 14:48:54 +0530

    mongosh

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -7,3 +7,4 @@ mongoose.connection.on('connected', function () { console.log('Mongoose connecte
 mongoose.connection.on('error', function (err) { console.log('Mongoose connection error: ' + err); });
 mongoose.connection.on('disconnected', function () { console.log('Mongoose disconnected'); });
 
+require('./reviews');
